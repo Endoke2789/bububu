16d6bbf5b2be6050ab461d59b9f7ac6c1cece5b2
Endoke2789 <148538738+Endoke2789@users.noreply.github.com>
2023-10-27 19:11:51 +0900

    Add files via upload

---
+++ 1027.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	printf("Please enter number?:");
+	scanf("%d", &a);
+	printf("Please enter other number?:");
+	scanf("%d", &b);
+	c = a * b;
+	printf("c=%d\n",c);
+	return 0;
+}
+
+
